edb9c6fec2e40b45980f9416763333223625568a
seansg <seansgarwood@gmail.com>
2022-12-28 12:02:42 -0500

    Add event listeners to buttons
    The round is played after a button is clicked and the results are
    returned to the console.

--- script.js
+++ script.js
@@ -30,15 +30,10 @@ that might look something like this:
 
 PSEUDOCODE
 
-fn gPC(args?) {
-  store the contents of the button clicked into a variable called choice and
-  return it.
-}
 
 declare three button vars
 add event listeners to each that returns some function on click.
   --what is that function?
-
 
 */
 
@@ -51,23 +46,18 @@ function getComputerChoice() {
   } else {return 'scissors';}
 }
 
-//prompts the user for a choice of rock, paper, or scissors
-function getPlayerChoice() {
-  return prompt('Rock, paper, or scissors?', 'rock').toLocaleLowerCase();
-}
-
 //plays a round, returns a string reporting results
 function playRound(playerSelection, computerSelection) {
   //nine possibilities, three ties, three wins, three losses
   if (playerSelection === computerSelection) {
-    return 'It\'s a tie';
+    return console.log('It\'s a tie');
   } else if (
     playerSelection === 'rock' && computerSelection === 'paper' 
   || playerSelection === 'paper' && computerSelection === 'scissors'
   || playerSelection === 'scissors' && computerSelection === 'rock'
   ) {
-    return 'You lose';
-  } else {return 'You win';}
+    return console.log('You lose');
+  } else return console.log('You win');
 }
 
 
@@ -76,29 +66,32 @@ function printResults(userScore, computerScore) {
   console.log('user score: ' + userScore + '\ncomputer score: ' + computerScore);
 }
 
-//plays five rounds and prints the results back to the console.
+/*
 function game() {
   let userScore = 0;
   let computerScore = 0; 
 
-  const choice = getPlayerChoice();
-  if (choice !== 'rock' && choice !== 'paper' && choice !== 'scissors') {
-    return game(); //starts game over if invalid input is entered by user
+  if 'It\'s a tie') {
+    console.log('It\'s a tie');
+    printResults(userScore, computerScore);
+  } else if (result === 'You lose') {
+    computerScore += 1;
+    console.log('You lose');
+    printResults(userScore, computerScore);
   } else {
-    let result = playRound(choice, getComputerChoice());
-    if (result === 'It\'s a tie') {
-      console.log('It\'s a tie');
-      printResults(userScore, computerScore);
-    } else if (result === 'You lose') {
-      computerScore += 1;
-      console.log('You lose');
-      printResults(userScore, computerScore);
-    } else {
-      userScore += 1;
-      console.log('You win');
-      printResults(userScore, computerScore)
-    }
+    userScore += 1;
+    console.log('You win');
+    printResults(userScore, computerScore)
   }
 }
+*/
 
-game(); //making this a fn call instead of a code block seems to be helpful, because it can be recursively called by itself.
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const compChoice = getComputerChoice();
+
+rock.addEventListener('click', e => playRound('rock', compChoice));
+paper.addEventListener('click', e => playRound('paper', compChoice));
+scissors.addEventListener('click', e => playRound('scissors', compChoice));
+//can't help but think there's a better way to do this.
