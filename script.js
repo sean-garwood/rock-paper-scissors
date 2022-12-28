b1cb5046f4701092bfe9ea85efed839784a42411
seansg <seansgarwood@gmail.com>
2022-12-28 13:43:05 -0500

    Remove game(), clean up OEPP

--- script.js
+++ script.js
@@ -1,39 +1,12 @@
-/*OUTLINE/REWRITE THE PROBLEM.
-
-add event listeners to each of the buttons.
-have the player make their choice based on which button is pressed.
-in other words, have getPlayerChoice return either 'rock', 'paper', or scissors
-
-need to do something like what the drum kit did:
-define an array-like object that selects all of the buttons in the document
-so that I can loop through them and add an event listener to each.
-
-or, more trivially, just add an event listener to the window that listens for
-clicks, then returns the text of the button element that was selected, and
-passes that to the playRound function.
-
+/*
+OUTLINE/REWRITE THE PROBLEM.
 
 
 ELEMENTS
 
-three buttons
-alerts win/lose/tie depending on result of the round
-
 PLAN
 
-I need to declare three variables, one for each button element in the document.
-i need to store the value of document.querySelector('#rock') into the rock
-variable/object.
-
-
-that might look something like this:
-
 PSEUDOCODE
-
-
-declare three button vars
-add event listeners to each that returns some function on click.
-  --what is that function?
 
 */
 
@@ -60,31 +33,10 @@ function playRound(playerSelection, computerSelection) {
   } else return console.log('You win');
 }
 
-
 //print results of the round to the console
 function printResults(userScore, computerScore) {
   console.log('user score: ' + userScore + '\ncomputer score: ' + computerScore);
 }
-
-/*
-function game() {
-  let userScore = 0;
-  let computerScore = 0; 
-
-  if 'It\'s a tie') {
-    console.log('It\'s a tie');
-    printResults(userScore, computerScore);
-  } else if (result === 'You lose') {
-    computerScore += 1;
-    console.log('You lose');
-    printResults(userScore, computerScore);
-  } else {
-    userScore += 1;
-    console.log('You win');
-    printResults(userScore, computerScore)
-  }
-}
-*/
 
 const buttons = document.querySelectorAll('button');
 
