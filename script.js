6fa9dc06dfd3e1df5ea3f6c3b84ec5e8723a6a51
seansg <seansgarwood@gmail.com>
2022-12-27 21:34:42 -0500

    Remove for-loop

--- script.js
+++ script.js
@@ -1,34 +1,12 @@
 /*OUTLINE/REWRITE THE PROBLEM.
 
-I must create a game of rock-paper-scissors that is to be played between a human and the computer. The game will be a best-of-five match, and the winner is the first player to reach three wins.
+remove logic that plays five rounds
 
-I will need to code an interface (which will be the console), a prompt for a user input of either rock, paper, or scissors, and a function that determines the computer's play (probably a random number generator), a function that will test for victory (conditional with three(?) branches), and an output to the console that reports the winner and loser.
 
 ELEMENTS
 
-I will need a basic html page (done), which links to script.js.
-
-I will need a script.js file, which will contain the logic and functions for the game.
-  specifically, i will need at least the following three functions:
-    getComputerChoice(): randomly returns rock/paper/scissors;
-    game(): contains the following function, as well as a prompt that reports the result of each match
-      playRound: Test for victory, using two parameters: playerSelection, computerSelection;
-    
 PLAN
 
-  enter game() function:
-  initialize wins vars? Or is this done in the for-loop as an index?
-    --don't we need two vars? compWins, humanWins?
-  for loop 
-    five rounds, index starts at zero and increments by one up to four.
-    fn 1.) generate random computer move
-    fn 2.) play round. return a string that will be console.logged. basically a big conditional statement
-      -nine possible combinations, three of which result in ties, and six of which result in a victor.
-        -if tie, go back to beginning
-        -if victor, return victor to console, as well as the total match score. increment either compWins or humanWins, goto beginning of fn
-  game ends when for-loop is exited:
-    print "congrats" or "sorry"
-    "play again?" (this might be challenging, idk)
 */
 
 //randomly generates one of three strings: 'rock', 'paper', or 'scissors'.
@@ -69,24 +47,22 @@ function game() {
   let userScore = 0;
   let computerScore = 0; 
 
-  for (let i = 0; i < 5; i++) {
-    const choice = getPlayerChoice();
-    if (choice !== 'rock' && choice !== 'paper' && choice !== 'scissors') {
-      return game(); //starts game over if invalid input is entered by user
+  const choice = getPlayerChoice();
+  if (choice !== 'rock' && choice !== 'paper' && choice !== 'scissors') {
+    return game(); //starts game over if invalid input is entered by user
+  } else {
+    let result = playRound(choice, getComputerChoice());
+    if (result === 'It\'s a tie') {
+      console.log('It\'s a tie');
+      printResults(userScore, computerScore);
+    } else if (result === 'You lose') {
+      computerScore += 1;
+      console.log('You lose');
+      printResults(userScore, computerScore);
     } else {
-      let result = playRound(choice, getComputerChoice());
-      if (result === 'It\'s a tie') {
-        console.log('It\'s a tie');
-        printResults(userScore, computerScore);
-      } else if (result === 'You lose') {
-        computerScore += 1;
-        console.log('You lose');
-        printResults(userScore, computerScore);
-      } else {
-        userScore += 1;
-        console.log('You win');
-        printResults(userScore, computerScore)
-      }
+      userScore += 1;
+      console.log('You win');
+      printResults(userScore, computerScore)
     }
   }
 }
